7745846b3ce4a2b6daa9758f468820ff7c448521
Brandon Villa <bornofos@gmail.com>
2017-08-16 16:01:45 -0500

    getAllObras method now is subscribed to service method and gets the data

--- src/app/pages/obras/components/obras-table/obras-table.component.ts
+++ src/app/pages/obras/components/obras-table/obras-table.component.ts
@@ -1,3 +1,5 @@
+import { ObrasInterface } from './obras.interface';
+import { ObrasResponseInterface } from './obras-response.interface';
 import { Component, OnInit } from '@angular/core';
 import { ObrasService } from './obras.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
@@ -7,15 +9,15 @@ import { ObrasEditModal } from './obras-edit-modal/obras-edit-modal.component';
 @Component({
   selector: 'obras-table',
   templateUrl: './obras-table.html',
-  styleUrls: ['./obras-table.scss']
+  styleUrls: ['./obras-table.scss'],
 })
 export class ObrasTable implements OnInit {
 
     data;
-    filterQuery = "";
+    filterQuery = '';
     rowsOnPage = 10;
-    sortBy = "nombre";
-    sortOrder = "asc";
+    sortBy = 'idobra';
+    sortOrder = 'asc';
 
     constructor(private service: ObrasService, private modalService: NgbModal) {
     }
@@ -27,6 +29,7 @@ export class ObrasTable implements OnInit {
     addObrasModalShow() {
       const activeModal = this.modalService.open(ObrasAddModal, { size: 'lg' });
       activeModal.componentInstance.modalHeader = 'Agregar Obra';
+      console.log('Add Modal opened');
     }
 
     editObrasModalShow(id: any) {
@@ -37,34 +40,29 @@ export class ObrasTable implements OnInit {
 
     }
     
-    onDeleteConfirm(event, item): void {
+    onDeleteConfirm(event, id): void {
       if (window.confirm('¿Estas seguro de querer eliminar este registro?')) {
-        console.log('item.id a borrar', item.id);
+        console.log('item.id a borrar', id);
       } else {
-        console.log('item.id cancelando', item.id);
+        console.log('item.id cancelando', id);
       }
     }
 
 
     ngOnInit() {
-        this.getAllObrass();
+        this.getAllObras();
     }
     
-    private getAllObrass(): void {
+    private getAllObras(): void {
 
-      this.service.getAllObrass().then((data) => {
-        this.data = data;
-      });
-
-
-      /*
-        this.service
-            .getAllObrass()
-            .subscribe(
-                (data:any[]) => console.log(data),
-                error => console.log(error),
-                () => console.log('Get all Items complete'));*/
-    }
-
-    
+      this.service
+          .getAllObras()
+          .subscribe(
+              (data: ObrasInterface[]) =>  {
+                this.data = data;
+                console.log(this.data);
+              },
+              error => console.log(error),
+              () => console.log('Get all Items complete'));
+    } 
 }
